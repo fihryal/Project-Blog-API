70a92c117574587a777feeb3de1251cbc9335265
fihryal <fihryalfaqiy@gmail.com>
2023-03-24 09:12:48 +0700

    activate Middleware

---
+++ app/Http/Middleware/PostOwner.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class PostOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        
+        $currentUser = Auth::user();
+        $post = post::findOrFail($request->id);
+
+        if($post -> author != $currentUser->id){
+            return response()->json([
+                'messages' => 'Data tidak di temukan'
+            ],404);
+        }
+        
+        return $next($request);
+    }
+}
